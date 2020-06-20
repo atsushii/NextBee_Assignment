e5537c64ad4035a3a57aecf6d2b4d63a3bfb0343
atsushiflee <atsushiflee@icloud.com>
2020-06-19 22:43:08 -0700

    Create initial employees dummy pool

---
+++ main.py
@@ -0,0 +1,13 @@
+from employee import employee as em
+
+
+def main():
+
+    # employees pool
+    dummy_employees_pool = [8, 3, 3, 2, 1, 10, 3]
+
+    employees = em.Employees(dummy_employees_pool)
+    print("start")
+
+
+if __name__ == "__main__"
