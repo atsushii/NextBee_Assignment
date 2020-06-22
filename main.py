2df2b6c6f648c9718f191be750904860593419a0
atsushiflee <atsushiflee@icloud.com>
2020-06-21 19:14:43 -0700

    Run worker

--- main.py
+++ main.py
@@ -1,13 +1,44 @@
-from employee import employee as em
+from employees.employees import Employees
+from jobs.jobs import Jobs
+import Queue as queue
+import threading
+from worker.worker import worker
 
 
 def main():
 
-    # employees pool
-    dummy_employees_pool = [8, 3, 3, 2, 1, 10, 3]
+    # initialize queue
+    q = queue.Queue()
 
-    employees = em.Employees(dummy_employees_pool)
-    print("start")
+    # initialize employees pool
+    employees = []
+
+    # Create employees pool
+    # keep job status and available hours
+    employees.append(Employees(True, 2))
+    employees.append(Employees(True, 3))
+    employees.append(Employees(True, 4))
+    employees.append(Employees(True, 5))
+    employees.append(Employees(True, 6))
+
+    sorted_order_employees_pool = sorted(employees, reverse=True)
+
+    # tread start
+    worker_tread = threading.Thread(
+        target=worker, args=(q, sorted_order_employees_pool))
+    worker_tread.setDaemon(True)
+    worker_tread.start()
+
+    # assigned job
+    q.put(Jobs(False, 5))
+    q.put(Jobs(False, 20))
+    q.put(Jobs(False, 1))
+    q.put(Jobs(False, 3))
+
+    q.join()
+
+    print("all work is done")
 
 
-if __name__ == "__main__"
+if __name__ == "__main__":
+    main()
